aff9bf188638b993616dd4d0a86049962fbd8903
doctording <dingwenlong2014@163.com>
2018-07-15 13:52:27 +0800

    add log4j

--- code/demo/src/main/java/com/example/demo/service/impl/TbUserServiceImpl.java
+++ code/demo/src/main/java/com/example/demo/service/impl/TbUserServiceImpl.java
@@ -9,19 +9,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class TbUserServiceImpl implements TbUserService{
+
+    final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     TbUserMapper tbUserMapper;
 
     @Override
     public String fortest(){
+        log.error("test-error-log");
         return "For Test";
     }
 
     @Override
     public List<TbUser> getAllUser() {
+        log.info("test-info-log");
         List<TbUser> userList = tbUserMapper.selectByExample(new TbUserExample());
         return userList;
     }
--- code/demo/src/test/java/com/example/demo/service/TbUserTest.java
+++ code/demo/src/test/java/com/example/demo/service/TbUserTest.java
@@ -4,15 +4,20 @@ import com.example.demo.repository.model.TbUser;
 import com.example.demo.service.TbUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import sun.rmi.runtime.Log;
 
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TbUserTest {
+
+    final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private TbUserService tbUserService;
@@ -24,6 +29,13 @@ public class TbUserTest {
         userList.forEach(tbUser -> {
             System.out.println(tbUser);
         });
+        System.out.println("==============");
+    }
+
+    @Test
+    public void testFortest(){
+        System.out.println("==============");
+        System.out.println(tbUserService.fortest());
         System.out.println("==============");
     }
 }
